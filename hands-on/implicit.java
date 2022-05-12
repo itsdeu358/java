12d01fc4a86f30a67a9ce50d5a47daacba6d61f1
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-05-12 12:45:33 +0530

    Add files via upload

---
+++ hands-on/implicit.java
@@ -0,0 +1,10 @@
+package corejava;
+
+public class implicit {
+public static void main (String[]args) {
+int i= 600;
+float f=i;
+System.out.println("int value"+i);
+System.out.println("float value"+f);
+}
+}
