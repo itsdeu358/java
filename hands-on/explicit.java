049fd62901a30d67f7fb3f4d90bf5498442e0cfd
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-05-12 13:07:21 +0530

    Add files via upload

---
+++ hands-on/explicit.java
@@ -0,0 +1,15 @@
+package corejava;
+
+public class explicit {
+public static void main(String[]args)
+{
+	double d=800.0;
+	float f=(float)d;
+	long l=(long)f;
+	int i=(int)l;
+	System.out.println("double value"+d);
+	System.out.println("float value"+f);
+	System.out.println("long value"+l);
+	System.out.println("int value"+i);
+}
+}
