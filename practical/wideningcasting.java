b8b9093c9313c8b0d88c6bc4bc48dd9a40394ffb
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-05-12 12:10:20 +0530

    Add files via upload

---
+++ practical/wideningcasting.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class wideningcasting {
+	public static void main(String []agrs)
+	{
+		int i = 200;
+		 
+		//automatic type conversion
+		long l = i;
+		 
+		//automatic type conversion
+		float f = l;
+		System.out.println("Int value "+i);
+		System.out.println("Long value "+l);
+		System.out.println("Float value "+f);
+		}
+		}
+
