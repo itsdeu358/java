5a6bec07c1d559ad13a8a70e5179f486b08b1b32
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-05-12 12:25:16 +0530

    Add files via upload

---
+++ practical/explicitcasting.java
@@ -0,0 +1,17 @@
+package corejava;
+//Java program to illustrate type casting 
+public class explicitcasting {
+	public static void main(String[]args)
+	
+	{
+	
+	byte b = 70;
+	 
+	b = (byte)(b * 2);
+	System.out.println(b);
+	}
+	}
+
+
+
+
