463f60d11a4252cb86adaaa77274a3a3b380ee8a
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-05-16 17:25:09 +0530

    Add files via upload

---
+++ practical/abstraction.java
@@ -0,0 +1,11 @@
+package corejava;
+
+//creating abstract class
+
+abstract class abstraction {
+
+	//creating variables
+	
+	public abstract int Multiply(int n1, int n2); 
+	public abstract int Multiplyone(int n1, int n2, int n3); 	
+}
