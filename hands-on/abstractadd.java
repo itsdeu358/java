42d8b967ada52fd568ad1f05a77078a7059b890a
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-05-16 17:28:33 +0530

    Add files via upload

---
+++ hands-on/abstractadd.java
@@ -0,0 +1,12 @@
+package corejava;
+
+//creating class
+
+abstract class abstractadd {
+
+	//creating variables
+	
+	public abstract int addition(int n1, int n2);
+	public abstract int substraction(int n1, int n2);
+	
+}
