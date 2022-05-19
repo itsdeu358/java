13b102e7c10c164aa2fc8f97a725e74c0d01551f
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-05-19 11:01:59 +0530

    Add files via upload

---
+++ practical/Continuestate.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class Continuestate {
+	
+	public static void main(String[] args) {
+		for (int i=0; i<10; i++) {
+		if(i==4) {
+			continue;
+		}
+		System.out.println(i);
+		}
+		
+	}
+}
+
+
