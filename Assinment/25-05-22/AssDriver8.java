ea254b9cf5c1d0053e3fc44af823c65dd11e5ea2
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-05-27 10:48:01 +0530

    Add files via upload

---
+++ Assinment/25-05-22/AssDriver8.java
@@ -0,0 +1,15 @@
+package corejava;
+
+public class AssDriver8 {
+	public static void main(String[] args)	{
+  	  int arr[][] = { { 11, 22, 66 }, { 33, 44, 55 } };
+   	 for (int i = 0; i < 2 ; i++) 
+{
+        for (int j = 0; j < 3; j++)
+        System.out.print(arr[i][j] + " ");
+        System.out.println();
+  	  }
+	  }
+}
+
+
