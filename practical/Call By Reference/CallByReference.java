f276a0efe2c59521d9c85cd5d0f63fbd6702ba7c
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-06-06 12:40:24 +0530

    Add files via upload

---
+++ practical/Call By Reference/CallByReference.java
@@ -0,0 +1,15 @@
+package corejava;
+//java program to demonstrate Call by reference 
+public class CallByReference {
+//The original value of 'a' will be changed as we are trying to pass the objects.
+//Objects are passed by reference.
+	int a =10;
+	void call(CallByReference c) {c.a =c.a+10;}
+	public static void main(String[]args) {
+		CallByReference c = new CallByReference();
+		System.out.println("Before call-by-reference:" + c.a);
+	//Passing the object as a value using pas-by-reference 
+		c.call(c);
+		System.out.println("After call-by-reference:" + c.a);
+	}
+}
