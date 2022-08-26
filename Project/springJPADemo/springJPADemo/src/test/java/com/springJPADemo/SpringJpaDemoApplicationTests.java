980855846bf6918b642a84b6cf35175bcc1ced4e
yogita desh <96690110+itsdeu358@users.noreply.github.com>
2022-08-26 13:03:58 +0530

    Add files via upload

---
+++ Project/springJPADemo/springJPADemo/src/test/java/com/springJPADemo/SpringJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springJPADemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
